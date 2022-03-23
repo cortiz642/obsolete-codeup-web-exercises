15c03b7570ecd5c79a6b6352c726a0cfc8335f45
Christopher Ortiz <icomputer@ChristohersiMac.attlocal.net>
2022-03-23 17:05:03 -0500

    first commit, added practice.js

---
+++ codeup-web-exercises/js/practice.js
@@ -0,0 +1,20 @@
+//TODO: Fizzbuzz Activity
+//Print numbers 1-100
+//if # is divisible by 3 use Fizz
+//if # is divisible by 5 use Buzz
+//if # is divisible by both 3 and 5 use FizzBuzz
+
+// function fizzBuzz(){
+//     for (var i = 1; i <= 100; i++){
+//         if(i % 3 === 0 && i % 5 === 0){
+//             console.log("FizzBuzz");
+//         }else if (i % 3 === 0){
+//             console.log("Fizz")
+//         }else if (i % 5 === 0){
+//             console.log("Buzz");
+//         }else{
+//             console.log(i); //print #'s 1-100
+//         }
+//     }
+// }
+// fizzBuzz();
