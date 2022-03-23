25d9c7669de3198b14016635ee5131c922be655c
Christopher Ortiz <icomputer@ChristohersiMac.attlocal.net>
2022-03-23 17:04:23 -0500

    first commit, added iterating.js

--- codeup-web-exercises/js/iterating.js
+++ codeup-web-exercises/js/iterating.js
@@ -6,42 +6,71 @@
      * Create an array of 4 people's names and store it in a variable called
      * 'names'.
      */
+    // name variable names
+    // names will have an array of 4 people's names
+
+    // var names = ['alpha', 'bravo', 'charly', 'delta'];
 
     /**
      * TODO:
      * Create a log statement that will log the number of elements in the names
      * array.
      */
+    // create a console.log names.length
+    // to push out the number of elements within the array
 
+    // console.log(names.length);
     /**
      * TODO:
      * Create log statements that will print each of the names individually by
      * accessing each element's index.
      */
+    // create console.log to print each element by name
 
+    // console.log(names[0]);
+    // console.log(names[1]);
+    // console.log(names[2]);
+    // console.log(names[3]);
     /**
      * TODO:
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
-
+    // write a for loop to include index & <names.length; i++
+    // console log every item in the names array
+    //
+    // for(var i = 0; i < names.length; i++) {
+    //     console.log('The names at index ' + i + ' is: ' + names[i]);
+    // }
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
      */
+    // Accomplish the above task by utilizing forEach
 
+    // names.forEach(function(names){
+    //     console.log('This name is: ' + names);
+    // })
     /**
-     * TODO:
-     * Create the following three functions, each will accept an array and
-     * return an an element from it
-     * - first: returns the first item in the array
-     * - second: returns the second item in the array
-     * - last: returns the last item in the array
-     *
-     * Example:
-     *  > first([1, 2, 3, 4, 5]) // returns 1
-     *  > second([1, 2, 3, 4, 5]) // returns 2
-     *  > last([1, 2, 3, 4, 5]) // return 5
-     */
+     // * TODO:
+     // * Create the following three functions, each will accept an array and
+     // * return an an element from it
+     // * - first: returns the first item in the array
+     // * - second: returns the second item in the array
+     // * - last: returns the last item in the array
+     // *
+     // * Example:
+     // *  > first([1, 2, 3, 4, 5]) // returns 1
+     // *  > second([1, 2, 3, 4, 5]) // returns 2
+     // *  > last([1, 2, 3, 4, 5]) // return 5
+     // */
+var first = [1, 2, 3, 4, 5];
+var second = [1, 2, 3, 4, 5];
+var third = [1, 2, 3, 4, 5];
+
+var numbers = [first, second, third];
+numbers.forEach(function(first){
+    return(1);
+})
 
 })();
