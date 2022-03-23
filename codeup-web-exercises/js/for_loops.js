4e586aab68ad9e0cea576453f88f87965cecb383
Christopher Ortiz <icomputer@ChristohersiMac.attlocal.net>
2022-03-22 19:55:16 -0500

    first commit, added for_loops.js

---
+++ codeup-web-exercises/js/for_loops.js
@@ -0,0 +1,44 @@
+// function showMultiplicationTable(num){
+//     for(var i = 1; i <= 10; i++){
+//         var result = num * i;
+//         console.log(num + ' x ' + i + ' = ' + result);
+//     }
+// }
+// showMultiplicationTable();
+
+
+// var numberToStop = 30
+// for (var i = 20; i < 200; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i + ' is even');
+//     } else if (i > 30) {
+//         break;
+//     } else {
+//         console.log(i + ' is odd');
+//     }
+// }
+
+// for(var i = 1; i <= 10; i++){
+//     var randNumber = Math.floor(Math.random() * (200 - 20) ) + 20;
+//     console.log(randNumber);
+//     if(randNumber % 2 == 0){
+//         console.log(randNumber + " is even")
+//     }else {
+//         console.log( randNumber + " is odd")
+//     }
+// }
+
+// for(var i=1; i<=9; i++){
+//     var Num='';
+//     for(var j=0; j<i; j++){
+//         Num += i;
+//     }
+//     console.log(Num) }
+
+// for(var i = 5; i < 100; i++){
+//     if(i % 5 === 0){
+//         console.log(i);
+//     }
+// }
+
+
