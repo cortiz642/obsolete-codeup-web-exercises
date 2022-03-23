265540a08c2ad7671642611d2293b8dad02316a9
Christopher Ortiz <icomputer@ChristohersiMac.attlocal.net>
2022-03-22 19:54:34 -0500

    first commit/added conditionals.js

--- codeup-web-exercises/js/conditionals.js
+++ codeup-web-exercises/js/conditionals.js
@@ -1,8 +1,11 @@
-"use strict";
+// "use strict";
 
 /* ########################################################################## */
 
 // /**
+// Research parameters and variables within functions and conditions
+// Information can flow inward not outward
+// Lookup functional programming
 //  * TODO:
 //  * Create a function named `analyzeColor` that accepts a string that is a color
 //  * name as input. This function should return a message which relates to the
@@ -24,21 +27,33 @@
 //     // 2. return a message referencing the color to something
 //     // 3. return a string
 
-// var analyzeColor = prompt("What is your favorite color");
-// var analyzeColor = ("blue, yellow, red, green");
-// function analyzeColor(blue, yellow, red, green) {
 //
-// }if(analyzeColor === blue){
-//         prompt("blue is the color of a car")
-//     }else if (analyzeColor === yellow){
-//         prompt("yellow is the color of some snow");
-//     }else if (analyzeColor === red){
+// var color = prompt("What is your favorite color");
+// var myColor = ['blue','yellow','red','green'];
+// var analyzeColor1 = ("blue");
+// var myColor2 = ("yellow");
+// var myColor3 = ("red");
+// var myColor4 = ("green");
+
+// var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+// var randomColor = colors[Math.floor(Math.random() * colors.length)];
+// function analyzeColor(inputColor) {
+//     if (inputColor === 'blue') {
+//         console.log("blue is the color of a car");
+//     } else if (inputColor === 'yellow') {
+//         console.log("yellow is the color of some snow");
+//     } else if (inputColor === 'red') {
 //         console.log("red is the color of roses");
-//     }else if (analyzeColor === green){
+//     } else if (inputColor === 'green') {
 //         console.log("green is the color of grass");
-//     }else {
+//     } else {
 //         console.log("Pick another color");
 //     }
+// }
+// analyzeColor(colors);
+
+
+
 
 
 // Don't change the next two lines!
@@ -54,22 +69,66 @@
 //  * You should see a different message every time you refresh the page
 //  */
 //
+//
+// function analyzeColor(inputColor) {
+//     if (inputColor === 'blue') {
+//         console.log("blue is the color of a car");
+//     } else if (inputColor === 'yellow') {
+//         console.log("yellow is the color of some snow");
+//     } else if (inputColor === 'red') {
+//         console.log("red is the color of roses");
+//     } else if (inputColor === 'green') {
+//         console.log("green is the color of grass");
+//     } else {
+//         console.log("Pick another color");
+//     }
+// }
+//     analyzeColor(randomColor);
 // /**
 //  * TODO:
 //  * Comment out the code above, and refactor your function to use a switch-case statement
 //  */
 
-// var moods = prompt("What kind of mood are you in?");
-// switch(analyzeColor){
-//     case
-}
+// switch (analyzeColor) {
+//
+//     case "blue":
+//         console.log("1. first case ---");
+//         alert("What a coincidence, that's my favorite color!");
+//         break;
+//     case "yellow":
+//         alert("Are you sure");
+//         break;
+//     default:
+//         alert(analyzeColor + "isn't my favorite, but it's all good!");
+//         break;
+
+// }
 // /**
+
+
 //  * TODO:
 //  * Prompt the user for a color when the page loads, and pass the input from the
 //  * user to your `analyzeColor` function. Alert the return value from your
 //  * function to show it to the user.
 //  */
-//
+
+// var color = prompt("What is your favorite color");
+// function analyzeColor(inputColor) {
+//     if (inputColor === 'blue') {
+//         alert("blue is the color of a car");
+//     } else if (inputColor === 'yellow') {
+//         alert("yellow is the color of some snow");
+//     } else if (inputColor === 'red') {
+//         alert("red is the color of roses");
+//     } else if (inputColor === 'green') {
+//         alert("green is the color of grass");
+//     } else {
+//         alert("Pick another color");
+//     }
+// }
+//     analyzeColor(color);
+
+
 // /* ########################################################################## */
 //
 // /**
@@ -91,21 +150,44 @@
 //  * Test your function by passing it various values and checking for the expected
 //  * return value.
 //  */
-var number = [0, .1, .25, .35, .5, 100];
-var randomNumber = number[Math.floor(Math.random() * number.length)];
-// var myNumber = randomNumber
-console.log(randomNumber);
+// var luckyNumber = Math.floor(Math.random() * 6);
+// var price = prompt("Enter total amount!");
+// var discount = ['.1', '.25', '.35', '.5'];
+// var disPrice = calculateTotal(luckyNumber, price);
+// var myDisc = (price - disPrice);
+// // var luckyNumber = [Math.floor(Math.random() * number.length)];
+// function calculateTotal(luckyNumber, totalAmount){
+//     var myResult = (luckyNumber);
+//     console.log(myResult)
+//     if(luckyNumber === 1){
+//         return(totalAmount * .1);
+//     }else if(luckyNumber === 2){
+//         return(totalAmount * .25);
+//     }else if(luckyNumber === 3){
+//         return(totalAmount * .35);
+//     }else if(luckyNumber === 4){
+//         return(totalAmount * .5);
+//     }else {
+//         return(totalAmount * 1)
+//     }
+// }
+// console.log(myDisc);
+//     calculateTotal(luckyNumber, discount);
+// var number = [0, .1, .25, .35, .5, 100];
+// var randomNumber = number[Math.floor(Math.random() * number.length)];
+// // var myNumber = randomNumber
+// console.log(randomNumber);
 // var zero = 0;
 // var one = .1;
 // var two = .25;
 // var three = .35;
 // var four = .5;
 // var five = 1;
-var totalBill = 100
-function calculateTotal(randomNumber, totalBill){
-    return (1 - randomNumber) * totalBill
-}
-console.log(calculateTotal(randomNumber, totalBill));
+// var totalBill = 100
+// function calculateTotal(randomNumber, totalBill){
+//     return (1 - randomNumber) * totalBill
+// }
+// console.log(calculateTotal(randomNumber, totalBill));
 // if (myNumber === zero){
 //  (1 - zero)
 //  (console.log(zero);
@@ -150,4 +232,14 @@ console.log(calculateTotal(randomNumber, totalBill));
 //  * Can you refactor your code to use functions?
 //  * HINT: The way we prompt for a value could be improved
 //  */
-//
+
+// 1. Prompt user to confirm yes or no to enter a number
+// 2. Prompt user to input a number (if the clicked ok)
+// 3. Prompt user with three alerts
+//  alert1) whether the number they input was even or odd
+//  alert2) what there number plus 100 is
+//  alert3) is the total number negative or positive
+// 4. Make an alert if the user inputs the wrong data type such as a letter, user won't
+// be able to go further until done so.
+
+var text;
