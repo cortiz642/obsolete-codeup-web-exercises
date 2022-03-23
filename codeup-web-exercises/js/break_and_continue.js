55045c5d034414f474ebe35d3e8b95ed87b8c57b
Christopher Ortiz <icomputer@ChristohersiMac.attlocal.net>
2022-03-22 19:52:57 -0500

    first commit, added break_and_continue.js

---
+++ codeup-web-exercises/js/break_and_continue.js
@@ -0,0 +1,21 @@
+// do {
+//     var userInput = prompt("Enter an odd number between 1 and 50.");
+//     }while(userInput % 2 === 0 || userInput < 1 || userInput > 50 || isNaN(userInput));
+
+
+
+// var userInput = prompt("Enter an odd number between 1 and 50.");
+// //     }while(userInput % 2 === 0 || userInput < 1 || userInput > 50 || isNaN(userInput));
+//
+// for(var i = 1; i <= 50; i++) {
+//     if (i == userInput) {
+//         console.log("Yikes! Skipping number " + userInput);
+//     } else if (i % 2 !== 0) {
+//         console.log("This is an odd number" + i);
+//     } else {
+//         continue;
+//
+//     }
+}
+
+
