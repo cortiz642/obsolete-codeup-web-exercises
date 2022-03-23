76099abf1b8374faccbbbaec4698bb61472fe125
Christopher Ortiz <icomputer@ChristohersiMac.attlocal.net>
2022-03-22 20:05:48 -0500

    first commit/added while.js

--- codeup-web-exercises/js/while.js
+++ codeup-web-exercises/js/while.js
@@ -14,3 +14,26 @@ do {
     console.log("amtToSell AFTER: ", amtToSell);
     console.log("amtBought: ", amtBought);
 }while (amtToSell > amtBought)
+
+
+
+var amtToSell = Math.floor(Math.random() * 50) + 50;
+do {
+    console.log("amtToSell Before: ", amtToSell);
+    var amtBought = Math.floor(Math.random() * 5) + 1;
+    amtToSell -= amtBought;
+
+    console.log("amtToSell AFTER: ", amtToSell);
+    console.log("amtBought: ", amtBought);
+}while (amtToSell > amtBought)
+
+
+var amtToSell = Math.floor(Math.random() * 50) + 50;
+do {
+    console.log("amtToSell Before: ", amtToSell);
+    var amtBought = Math.floor(Math.random() * 5) + 1;
+    amtToSell -= amtBought;
+
+    console.log("amtToSell AFTER: ", amtToSell);
+    console.log("amtBought: ", amtBought);
+}while (amtToSell > amtBought)
