ec761f6f2645b0bc06be08f3b25954f8687d3d54
Christopher Ortiz <icomputer@ChristohersiMac.attlocal.net>
2022-03-27 17:37:43 -0500

    second commit, revised external.js

--- codeup-web-exercises/js/external.js
+++ codeup-web-exercises/js/external.js
@@ -1,1 +1,53 @@
-console.log("Hello from external JavaScript")
+// console.log("Hello from external JavaScript");
+
+// alert("Welcome to my Website!");
+
+// var userResponse = prompt("What is your favorite color?");
+// alert("Awesome! " + userResponse + " That's my favorite color too!");
+
+//
+// var userInput = prompt("What movie would you like to rent? " + "The little mermaid " +
+// " Brother Bear " + "or" + " Hercules");
+// alert("Great you have chose " + userInput);
+// var rentalDays = prompt("How many days would you like to rent?");
+// alert("Great! " + "Your are renting " + userInput + " for " + rentalDays + " Days");
+
+
+// var place1 = prompt("How many hours did you work for Google? ");
+// var place2 = prompt("How many hours did you work for Amazon?");
+// var place3 = prompt("How many hours did you work for Facebook?");
+// var googlePay = 400;
+// var amazonPay = 380;
+// var facebookPay = 350;
+// alert("You earned " + place1 * googlePay, + "for Google " + place2 * amazonPay,
+//     + " for Amazon " + place3 * facebookPay, + " for Facebook");
+
+// var schedule = prompt("Are you able to enroll in a class?");
+// var enroll = prompt("Is the class full?");
+// var issue = prompt("Will this interfere with your current schedule?")
+// var abletoEnroll = ("no");
+// if(enroll === abletoEnroll && issue === abletoEnroll){
+//     alert("You can enroll!");
+// }else{
+//     alert("You can not enroll at this time!");
+// }
+
+// var customer = prompt("Are you a premium member?")
+//
+// if(customer === 'yes') {
+//     var customer2 = prompt("Has the offer expired?");
+// }  else if (customer2 !== 'no') {
+//         alert("Offer expired");
+//     } else {
+//         alert("Thank you!");
+//     }
+//     if (customer === 'no') {
+//         var customer1 = prompt("Are you purchasing more than 2 items?");
+//         console.log();
+//     }if (customer1 !== 'yes') {
+//             alert("Offer expired!")
+//         } else {
+//             alert("Thank you!")
+//         }
+
+
