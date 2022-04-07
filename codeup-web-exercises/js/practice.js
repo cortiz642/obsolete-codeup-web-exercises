e87140bc6e0f7dde1da17f55f8a91f05be388665
cortiz642 <chris.o.ortiz93@gmail.com>
2022-04-06 21:05:58 -0500

    modified practice.js

--- codeup-web-exercises/js/practice.js
+++ codeup-web-exercises/js/practice.js
@@ -150,6 +150,30 @@
 // }
 // sumArr(passedNumbers);
 
+///////////////////// MORNING WARMUP 4/6/2022 ///////////////////////////////
+// example input: [5,8,2,6,9,3,2];
+// expected output: [2,2,3,5,6,8,9] // length: 7
+// some possible test cases to think about:
+//     should be called ‘sortArr’
+// function should return an array
+// function should accept an array
+// BONUS thoughts
+// what would happen if you had negative numbers in your list
+// can you sort and then remove any duplicates (would it be more/less efficient to remove duplicates and then sort?)
+// if your array has non-numbers can you still sort the numbers? ex: input: [3,“b”,8,5,9,true”, 4,“xyz”] expected output: [3,4,5,8,9];
+
+// var sortArr = [5, 8, 2, 6, 9, 3, 2];
+// sortArr.sort();
+// console.log(sortArr);
+
+
+/////BONUS
+// var sortArr = [3, "b", 8, 5, 9, "true”, 4,“xyz”];
+
+
+
+
+
 
 
 
