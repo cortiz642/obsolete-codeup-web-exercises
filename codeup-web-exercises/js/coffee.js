d04a276f43fdbd9850c378aa5c4c933b6ad70eab
cortiz642 <chris.o.ortiz93@gmail.com>
2022-04-27 11:35:39 -0500

    modified

---
+++ codeup-web-exercises/js/coffee.js
@@ -0,0 +1,91 @@
+"use strict"
+
+// changes tbody text to read out coffee blends
+function renderCoffee(coffee) {
+    var html = '<div class="coffee d-flex align-items-center">';
+//     html += '<td>' + coffee.id + '</td>';
+    html += '<h2 class="name-style my-1 mr-2">' + coffee.name + '</h2>';
+    html += '<p class="roast-style align-self-end mb-2">' + coffee.roast.toUpperCase() + '</p>';
+    html += '</div>';
+
+    return html;
+}
+
+//loops through each blend to add new html for each one to print
+function renderCoffees(coffees) {
+    var html = '';
+    coffees.forEach(function(blend) {
+        html += renderCoffee(blend);
+    })
+    return html;
+}
+
+function updateCoffees(e) {
+    e.preventDefault(); // don't submit the form, we just want to update the data
+    var selectedRoast = roastSelection.value;
+    var selectedName = nameSelection.value;
+    var filteredCoffees = [];
+    coffees.forEach(function (coffee) {
+        if (coffee.name.toUpperCase().indexOf(selectedName.toUpperCase()) > -1) {
+            filteredCoffees.push(coffee);
+        }
+            // if (selectedRoast === 'all' && selectedName === '') {
+            //     filteredCoffees.push(coffee);
+            //     if (coffee.name !== selectedName && selectedName !== '') {
+            //         filteredCoffees.splice(filteredCoffees.indexOf(coffee), 1);
+            //     }
+            // } else if (selectedRoast === 'all' && selectedName !== '') {
+            //     filteredCoffees.push(coffee);
+            //     if (coffee.name !== selectedName) {
+            //         filteredCoffees.splice(filteredCoffees.indexOf(coffee), 1);
+            //     }
+            // } else if (coffee.roast === selectedRoast) {
+            //     filteredCoffees.push(coffee);
+            //     if (coffee.name !== selectedName && selectedName !== '') {
+            //         filteredCoffees.splice(filteredCoffees.indexOf(coffee), 1);
+            //     }
+            // }
+            tbody.innerHTML = renderCoffees(filteredCoffees);
+    });
+}
+function newButton(input){
+    if(input)
+
+        var coffeeObject= {
+            name:submitNew,
+            roast:roastType,
+
+        }
+
+
+// from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
+    var coffees = [
+        {id: 1, name: 'Light City', roast: 'light'}, //each object is called 'blend'
+        {id: 2, name: 'Half City', roast: 'light'},
+        {id: 3, name: 'Cinnamon', roast: 'light'},
+        {id: 4, name: 'City', roast: 'medium'},
+        {id: 5, name: 'American', roast: 'medium'},
+        {id: 6, name: 'Breakfast', roast: 'medium'},
+        {id: 7, name: 'High', roast: 'dark'},
+        {id: 8, name: 'Continental', roast: 'dark'},
+        {id: 9, name: 'New Orleans', roast: 'dark'},
+        {id: 10, name: 'European', roast: 'dark'},
+        {id: 11, name: 'Espresso', roast: 'dark'},
+        {id: 12, name: 'Viennese', roast: 'dark'},
+        {id: 13, name: 'Italian', roast: 'dark'},
+        {id: 14, name: 'French', roast: 'dark'},
+    ];
+
+    var tbody = document.querySelector('#coffees');
+    var submitButton = document.querySelector('#submit');
+    var roastSelection = document.querySelector('#roast-selection');
+    var nameSelection = document.querySelector('#coffee-name');
+    var submitNew = document.querySelector('#submit-new')
+    var roastType = document.querySelector('#new-roast')
+
+    tbody.innerHTML = renderCoffees(coffees);
+
+    submitButton.addEventListener('click', updateCoffees);
+    roastSelection.addEventListener('change', updateCoffees);
+    nameSelection.addEventListener('keyup', updateCoffees);
+
--- codeup-web-exercises/js/jquery_exercise.js
+++ codeup-web-exercises/js/jquery_exercise.js
@@ -1,6 +1,9 @@
-var content = $('#frame').css('background-color', 'blue').html();
-alert(content);
+// var content = $('#frame').css('background-color', 'blue').html();
+// alert(content);
+//
+//
+// $('.list').css('background-color', 'yellow');
+// $('.codeup').css('border', '1px solid red');
 
-
-$('.list').css('background-color', 'yellow');
-$('.codeup').css('border', '1px solid red');
+$('li').css('font-size', '20px');
+$('h1,p,li').css('border', '1px solid red');
